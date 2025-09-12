db6d41a597921fc522cf85e2eec13b45e03087a3
kedarlute <65060000+kedarlute@users.noreply.github.com>
2025-09-12 15:36:52 +0530

    Update GoodsReceipt.java

--- src/test/java/tests/GoodsReceipt.java
+++ src/test/java/tests/GoodsReceipt.java
@@ -1,1 +1,33 @@
 
+package tests;
+
+/**
+ * @author: Kedarnath Lute
+ */
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class GoodReceipt {
+    WebDriver driver;
+
+   
+
+    @Test(priority = 1)
+    public void goods() {
+        driver.findElement(By.id("menu_inbound")).click();
+        driver.findElement(By.id("ok")).click();
+        
+        Assert.assertTrue(confirmation.isDisplayed(), "Sucess");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
+
