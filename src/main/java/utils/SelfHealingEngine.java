be9ba92b5af88e82fde461f8ae2e400debdd988e
kedarlute <65060000+kedarlute@users.noreply.github.com>
2025-12-30 11:21:38 +0530

    Add SelfHealingEngine class for automated actions

---
+++ src/main/java/utils/SelfHealingEngine.java
@@ -0,0 +1,19 @@
+import java.nio.file.*;
+import java.io.IOException;
+
+public class SelfHealingEngine {
+
+    public static void main(String[] args) throws IOException {
+        String prediction = Files.readString(Path.of(args[0])).toLowerCase();
+
+        if (prediction.contains("infra_timeout")) {
+            System.out.println("ACTION=RETRY");
+        }
+        else if (prediction.contains("service_down")) {
+            System.out.println("ACTION=RESTART_SERVICE");
+        }
+        else {
+            System.out.println("ACTION=ABORT");
+        }
+    }
+}
